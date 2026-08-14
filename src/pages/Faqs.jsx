06f48a9429524eab2f6c63d869c7751c0f08d2fa
bassit <bassitabimbola009@gmail.com>
2026-08-14 23:55:34 +0000

    second step

--- src/pages/Faqs.jsx
+++ src/pages/Faqs.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from "react"
 import gsap from "gsap"
 import { ScrollTrigger } from "gsap/ScrollTrigger"
-import FAQItem from "../components/ui/FAQItem"
+import FAQItem from "../components/ui/FAQitem"
 import work from "../assets/Data/Work.json"
 
 
